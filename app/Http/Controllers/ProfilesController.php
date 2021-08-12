5d1492d5cb308a5b201f3539dac9e6a05c644576
unknown <petyoruzhin@gmail.com>
2021-08-12 13:51:26 +0300

    update framework

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\Cache;
-use Intervention\Image\Facades\Image;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class ProfilesController extends Controller
 {
